5b87c18f6b090cd3affbef3b4207d514cc0db8eb
Larissa-Nahan <larissanahan@gmail.com>
2022-04-19 21:46:26 -0300

    Score, iniciar e reset

--- script.js
+++ script.js
@@ -13,16 +13,25 @@ let movePlayer2 = "";
 const $winnerTitle = document.querySelector(".winner-title");
 let gameResult = null;
 
+const $scorePlayer1 = document.querySelector(".score-player-1");
+const $scorePlayer2 = document.querySelector(".score-player-2");
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
+const $buttonReset = document.querySelector(".button-reset");
+const $buttonStart = document.querySelector(".button-start");
+let gameStart = false;
+
 function verifyWinner() {
-  if (movePlayer1 == "stone" && movePlayer2 == "paper") {
+  if (movePlayer1 == "rock" && movePlayer2 == "paper") {
     gameResult = 2;
-  } else if (movePlayer1 == "stone" && movePlayer2 == "scissors") {
+  } else if (movePlayer1 == "rock" && movePlayer2 == "scissors") {
     gameResult = 1;
   } else if (movePlayer1 == "paper" && movePlayer2 == "scissors") {
     gameResult = 2;
-  } else if (movePlayer1 == "paper" && movePlayer2 == "stone") {
+  } else if (movePlayer1 == "paper" && movePlayer2 == "rock") {
     gameResult = 1;
-  } else if (movePlayer1 == "scissors" && movePlayer2 == "stone") {
+  } else if (movePlayer1 == "scissors" && movePlayer2 == "rock") {
     gameResult = 2;
   } else if (movePlayer1 == "scissors" && movePlayer2 == "paper") {
     gameResult = 1;
@@ -43,40 +52,107 @@ function printGameResult() {
   }
 }
 
+function resetBattleFiel() {
+  $fieldPlayer1.innerHTML = "";
+  $fieldPlayer2.innerHTML = "";
+  $winnerTitle.innerHTML = "Resultado";
+}
+
+function resetMoveVariables() {
+  movePlayer1 = "";
+  movePlayer2 = "";
+}
+
+function resetScoresVariables() {
+  scorePlayer1 = 0;
+  scorePlayer2 = 0;
+}
+
+function printScoreBoard() {
+  if (scorePlayer1 < 10) {
+    $scorePlayer1.innerHTML = "0" + scorePlayer1;
+  }
+  if (scorePlayer2 < 10) {
+    $scorePlayer2.innerHTML = "0" + scorePlayer2;
+  }
+  if (scorePlayer1 >= 10) {
+    $scorePlayer1.innerHTML = scorePlayer1;
+  }
+  if (scorePlayer2 >= 10) {
+    $scorePlayer2.innerHTML = scorePlayer2;
+  }
+}
+
+function addPoint(winnerNumber) {
+  if (winnerNumber == 1) {
+    scorePlayer1++;
+  }
+  if (winnerNumber == 2) {
+    scorePlayer2++;
+  }
+  printScoreBoard();
+}
+
+function resetScoreBoard() {
+  $scorePlayer1.innerHTML = "00";
+  $scorePlayer2.innerHTML = "00";
+}
+
+function move(moveName, fieldNumber) {
+  if (gameStart) {
+    if (fieldNumber == 1) {
+      $fieldPlayer1.innerHTML =
+        '<img src="./' + moveName + '.png" class="move-image">';
+      movePlayer1 = moveName;
+    } else {
+      $fieldPlayer2.innerHTML =
+        '<img src="./' + moveName + '.png" class="move-image">';
+      movePlayer2 = moveName;
+    }
+    verifyWinner();
+    if (gameResult != null) {
+      setTimeout(resetBattleFiel, 2000);
+      resetMoveVariables();
+      addPoint(gameResult);
+      gameResult = null;
+    }
+  }
+}
+
 $rockButtonP1.addEventListener("click", function () {
-  $fieldPlayer1.innerHTML =
-    '<img src="./rock.png" alt="Mão fechada" class="move-image">';
-  movePlayer1 = "stone";
-  verifyWinner();
+  move("rock", 1);
 });
 $paperButtonP1.addEventListener("click", function () {
-  $fieldPlayer1.innerHTML =
-    '<img src="./paper.png" alt="Mão aberta" class="move-image">';
-  movePlayer1 = "paper";
-  verifyWinner();
+  move("paper", 1);
 });
 $scissorsButtonP1.addEventListener("click", function () {
-  $fieldPlayer1.innerHTML =
-    '<img src="./scissors.png" alt="Mão fechada com os dedos indicador e médio levantados" class="move-image">';
-  movePlayer1 = "scissors";
-  verifyWinner();
+  move("scissors", 1);
 });
 
 $rockButtonP2.addEventListener("click", function () {
-  $fieldPlayer2.innerHTML =
-    '<img src="./rock.png" alt="Mão fechada" class="move-image">';
-  movePlayer2 = "stone";
-  verifyWinner();
+  move("rock", 2);
 });
 $paperButtonP2.addEventListener("click", function () {
-  $fieldPlayer2.innerHTML =
-    '<img src="./paper.png" alt="Mão aberta" class="move-image">';
-  movePlayer2 = "paper";
-  verifyWinner();
+  move("paper", 2);
 });
 $scissorsButtonP2.addEventListener("click", function () {
-  $fieldPlayer2.innerHTML =
-    '<img src="./scissors.png" alt="Mão fechada com os dedos indicador e médio levantados" class="move-image">';
-  movePlayer2 = "scissors";
-  verifyWinner();
+  move("scissors", 2);
+});
+
+$buttonReset.addEventListener("click", function () {
+  resetBattleFiel();
+  resetMoveVariables();
+  resetScoreBoard();
+  resetScoresVariables();
+  gameResult = null;
+});
+
+$buttonStart.addEventListener("click", function () {
+  if (!gameStart) {
+    gameStart = true;
+    $buttonStart.classList.add("start");
+  } else {
+    gameStart = false;
+    $buttonStart.classList.remove("start");
+  }
 });
